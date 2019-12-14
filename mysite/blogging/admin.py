5454582481fe916a7a386e42163f24a80668e819
Paul Jurek <pjurek3@gmail.com>
2019-12-14 09:08:07 -0800

    formatted with black

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
 class CategoryInline(admin.TabularInline):
-    model = Category.posts.through 
+    model = Category.posts.through
+
 
 class PostAdmin(admin.ModelAdmin):
     inlines = [CategoryInline]
 
+
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [CategoryInline]
-    exclude = ('posts',)
+    exclude = ("posts",)
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
