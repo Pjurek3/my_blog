b01ba66a6422b27c72f02b2036df4821c544c974
Paul Jurek <pjurek3@gmail.com>
2019-12-21 10:19:15 -0800

    created initital serizler setup in app for Post

---
+++ mysite/blogging/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from blogging.models import Post
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['url', 'title', 'text', 'author', 'created_date', 'modified_date', 'published_date']
