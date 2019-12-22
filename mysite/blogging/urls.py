6ecc3440d58510ad2da122f284e346b366f62145
Paul Jurek <pjurek3@gmail.com>
2019-12-22 15:37:26 -0800

    implemented rss feed at the latest/feeds path.

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from blogging.views import detail_view, list_view, PostViewSet, UserViewSet, CategoryViewSet
 from rest_framework import routers
+from blogging.feeds import LatestPostsFeed
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -11,4 +12,5 @@ urlpatterns = [
     path("", list_view, name="blog_index"),
     path("api/", include(router.urls)),
     path("posts/<int:post_id>/", detail_view, name="blog_detail"),
+    path('latest/feed/', LatestPostsFeed()),
 ]
